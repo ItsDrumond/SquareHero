c762a884bbdc8ef208d4e5980406a9a71e7338e1
Bruno Drumond <BrunoDrumond@MacBook-Air-de-Patricia.local>
2022-10-06 22:53:32 +0100

    Moving X

--- src/main/java/com/brunodrumond/hero/Application.java
+++ src/main/java/com/brunodrumond/hero/Application.java
@@ -1,17 +1,15 @@
 package com.brunodrumond.hero;
 
-import com.googlecode.lanterna.TerminalSize;
-import com.googlecode.lanterna.TextCharacter;
-import com.googlecode.lanterna.screen.Screen;
-import com.googlecode.lanterna.screen.TerminalScreen;
-import com.googlecode.lanterna.terminal.DefaultTerminalFactory;
-import com.googlecode.lanterna.terminal.Terminal;
-
 import java.io.IOException;
 
 public class Application {
     public static void main(String[] args) throws IOException{
-        Game game = new Game();
-        game.run();
+        try {
+            Game game = new Game();
+            game.run();
+        }
+        catch (IOException e){
+            e.printStackTrace();
+        }
     }
 }
--- src/main/java/com/brunodrumond/hero/Game.java
+++ src/main/java/com/brunodrumond/hero/Game.java
@@ -2,41 +2,67 @@ package com.brunodrumond.hero;
 
 import com.googlecode.lanterna.TerminalSize;
 import com.googlecode.lanterna.TextCharacter;
+import com.googlecode.lanterna.input.KeyType;
 import com.googlecode.lanterna.screen.Screen;
+import com.googlecode.lanterna.input.KeyStroke;
 import com.googlecode.lanterna.screen.TerminalScreen;
 import com.googlecode.lanterna.terminal.DefaultTerminalFactory;
 import com.googlecode.lanterna.terminal.Terminal;
 
+import javax.swing.*;
 import java.io.IOException;
 
 public class Game {
     private Screen screen;
-    public Game(){
-        try {
-            TerminalSize terminalSize = new TerminalSize(40, 20);
-            DefaultTerminalFactory terminalFactory = new
-                    DefaultTerminalFactory()
-                    .setInitialTerminalSize(terminalSize);
-            Terminal terminal = terminalFactory.createTerminal();
+    private int x = 10;
+    private int y = 10;
 
-            screen = new TerminalScreen(terminal);
+    private void processKey(KeyStroke key) throws IOException{
 
-            screen.setCursorPosition(null);
-            screen.startScreen();
-            screen.doResizeIfNecessary();
-
-        } catch (IOException e) {
-            e.printStackTrace();
+        if(key.getKeyType() == KeyType.ArrowUp){
+            y--;
+        }
+        else if(key.getKeyType() == KeyType.ArrowDown){
+            y++;
+        }
+        else if(key.getKeyType() == KeyType.ArrowLeft){
+            x--;
+        }
+        else if(key.getKeyType() == KeyType.ArrowRight){
+            x++;
+        }
+        else if(key.getKeyType()==KeyType.Character && key.getCharacter() == 'q'){
+            screen.close();
         }
     }
 
     private void draw() throws IOException{
         screen.clear();
-        screen.setCharacter(10, 10, TextCharacter.fromCharacter('X') [0]);
+        screen.setCharacter(x, y, TextCharacter.fromCharacter('X') [0]);
         screen.refresh();
     }
 
     public void run() throws IOException{
-        draw();
+
+        TerminalSize terminalSize = new TerminalSize(40, 20);
+        DefaultTerminalFactory terminalFactory = new
+                DefaultTerminalFactory()
+                .setInitialTerminalSize(terminalSize);
+        Terminal terminal = terminalFactory.createTerminal();
+
+        screen = new TerminalScreen(terminal);
+
+        screen.setCursorPosition(null);
+        screen.startScreen();
+        screen.doResizeIfNecessary();
+
+        while(true) {
+            draw();
+            KeyStroke key = screen.readInput();
+            if(key.getKeyType() == KeyType.EOF){
+                break;
+            }
+            processKey(key);
+        }
     }
 }
